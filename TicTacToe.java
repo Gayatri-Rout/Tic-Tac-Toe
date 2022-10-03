807ea15276495253d21dcbb85f94c3cad98ee011
Gayatri Rout <grout130202@gmail.com>
2022-10-03 19:28:11 +0530

    vars declared

--- TicTacToe.java
+++ TicTacToe.java
@@ -4,6 +4,17 @@ import java.util.*;
 import javax.swing.*;
 
 public class TicTacToe implements ActionListener{
+
+    // whos turn is first?
+    Random random = new Random();
+
+    JFrame frame = new JFrame();
+    JPanel title_panel = new JPanel();
+    JPanel btn_panel = new JPanel();
+    JLabel textfield = new JLabel();
+    JButton[] btns = new JButton[9];
+    boolean player1_turn;
+
     // constructor for this class
     TicTacToe(){
 
