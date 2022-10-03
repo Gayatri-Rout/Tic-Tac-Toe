9d7253b41f5f53d3619cbeefd3bac07bbd3b38f7
Gayatri Rout <grout130202@gmail.com>
2022-10-03 19:24:27 +0530

    main class and tictactoe class initiated

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        // create an instance of TicTacToe class
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class TicTacToe implements ActionListener{
+    // constructor for this class
+    TicTacToe(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    // who starts the game?
+    public void firstTurn(){
+
+    }
+
+    // check to see who is winning
+    public void check(){
+
+    }
+
+    public void Xwins(int a, int b, int c){
+
+    }
+
+    public void Ywins(int a, int b, int c){
+
+    }
+
+
+}
